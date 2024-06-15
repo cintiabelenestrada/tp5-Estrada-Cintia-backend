accc53c33091d33af76060fefdc80fd7aec5c829
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-06-15 02:37:09 -0300

    se cambia el dni como string

--- models/espectador.js
+++ models/espectador.js
@@ -3,7 +3,7 @@ const { Schema } = mongoose;
 const EspectadorSchema = new Schema({
    apellido: { type: String, required: true },
    nombre: { type: String, required: true },
-   dni: { type: Number, required: true },
+   dni: { type: String, required: true },
    email: { type: String, required: true }
 })
 module.exports = mongoose.models.Espectador || mongoose.model('Espectador', EspectadorSchema);
